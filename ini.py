72b93205e4c1224b0326658f3467f37b33dae804
salahquddomi <44346406+salahquddomi@users.noreply.github.com>
2018-10-21 22:00:09 +0300

    Create ini.py

---
+++ ini.py
@@ -0,0 +1,6 @@
+
+__author__ = """salah.quddomi@hotmail.com"""
+__email__ = 'salah.quddomi@hotmail.com'
+__version__ = '1.2.3'
+
+from .api import load_image_file, face_locations, batch_face_locations, face_landmarks, face_encodings, compare_faces, face_distance
